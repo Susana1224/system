327a4d27accc58a3b7b6bb175aef87aca5e305e6
Susana1224 <duranosorio22@gmail.com>
2024-11-19 17:15:26 -0500

    Add files via upload

---
+++ src/routes/paginap/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import "$lib/css/paginap.css";
+</script>
+
+<main>
+	<div class="container"><h1>Gestión de Inventario</h1>
+
+		<div class="form-container">
+			<h2>Agregar Nombre del Estudiante</h2>
+			<input type="text" id="productName" placeholder="Nombre del Estudiante" />
+			<input type="number" id="productQuantity" placeholder="id" />
+			<input type="number" id="productQuantity" placeholder="Cantidad de complementos" />
+			<button type="submit" class="save">GUARDAR</button>
+			<button type="button" class="remove">ELIMINAR</button>
+		</div>
+	</div>
+</main>
+
+<style>
+</style>
